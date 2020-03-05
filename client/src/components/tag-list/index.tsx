88a000e7b7ac5fdd2bcde375ccc036b592f61e41
zacanger <zac@zacanger.com>
2020-03-05 11:09:20 -0700

    chore: flatten tag list dir, start work for filtering by tags/search in url

--- client/src/components/tag-list/index.tsx
+++ client/src/components/tag-list/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import styled from 'styled-components/macro'
-import TagList from './tag-list'
+import TagItem from './item'
+import { connect } from 'react-redux'
+import { fetchTags } from '../../actions/tags'
 
-const Wrapper = styled.aside`
+const Aside = styled.aside`
   margin-bottom: 16px;
   margin-right: 16px;
   border: 1px solid ${(props) => props.theme.border};
@@ -19,10 +21,51 @@ const Wrapper = styled.aside`
   }
 `
 
-const TagListWrapper = () => (
-  <Wrapper>
-    <TagList />
-  </Wrapper>
-)
+const TagListWrapper = styled.nav`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+`
 
-export default TagListWrapper
+const mapTags = (tags) =>
+  tags.map((tag, index) => <TagItem key={index} tag={tag} />)
+
+type TagListProps = {
+  fetchTags: () => void
+  isFetching: boolean
+  tags: string[]
+}
+
+export class TagList extends React.Component<TagListProps> {
+  componentDidMount() {
+    this.props.fetchTags()
+  }
+
+  render() {
+    if (this.props.isFetching) {
+      return null
+    }
+
+    /* eslint-disable fp/no-mutating-methods */
+    return (
+      <Aside>
+        <TagListWrapper>
+          {mapTags([
+            'all',
+            ...[...this.props.tags].sort((a, b) => a.localeCompare(b)),
+          ])}
+        </TagListWrapper>
+      </Aside>
+    )
+    /* eslint-enable fp/no-mutating-methods */
+  }
+}
+
+const mapStateToProps = (state) => ({
+  tags: state.tags.items,
+  isFetching: state.tags.isFetching,
+})
+
+const mapDispatchToProps = { fetchTags }
+
+export default connect(mapStateToProps, mapDispatchToProps)(TagList)
