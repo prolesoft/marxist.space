4d34c1cdb2a68a73259906f78d35947ca8328d7a
zacanger <zac@zacanger.com>
2020-07-11 11:21:31 -0600

    feat: goodbye, server

--- scripts/deployment/build-docker.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-version=$(git describe --exact 2>/dev/null || git rev-parse --short=10 HEAD)
-docker build -t marxist.space:$version -f scripts/deployment/Dockerfile .
