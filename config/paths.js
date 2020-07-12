7fc20d0943d15a8a9fd4b4871f477d29d7f24ecf
zacanger <zac@zacanger.com>
2020-07-11 18:00:13 -0600

    chore: get build working

--- config/paths.js
+++ config/paths.js
@@ -61,7 +61,7 @@ const resolveModule = (resolveFn, filePath) => {
 
 module.exports = {
   appPath: resolveApp('.'),
-  appBuild: resolveApp('build'),
+  appBuild: resolveApp('docs'),
   appPublic: resolveApp('public'),
   appHtml: resolveApp('public/index.html'),
   appIndexJs: resolveModule(resolveApp, 'src/index'),
